2ada4fe0a504f8405839d34fca1fd56796427109
masonkarsevar <masonkarsevar@gmail.com>
2019-09-09 12:04:39 -0700

    basic route is working. WIP: accounts get request

--- server.js
+++ server.js
@@ -6,4 +6,18 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.status(200).send('Server is working!!!')
+})
+
+server.get('/accounts', (req, res) => {
+    db('accounts')
+        .then(results => {
+            res.status(200).json(results);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+}); 
+
 module.exports = server;
