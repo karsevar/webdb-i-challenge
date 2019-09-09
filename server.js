4f3c49072f92edbb7fb76a39490b33dbb30283c8
masonkarsevar <masonkarsevar@gmail.com>
2019-09-09 12:26:15 -0700

    validateId middleware finished with individual get functionality

--- server.js
+++ server.js
@@ -18,7 +18,18 @@ server.get('/accounts', (req, res) => {
         .catch(error => {
             res.status(500).json(error);
         })
-}); 
+});
+
+server.get('/accounts/:id', validateId, (req, res) => {
+    db('accounts').where({id: req.params.id})
+        .first()
+        .then(results => {
+            res.status(200).json(results);
+        })
+        .catch(error => {
+            res.status(500).json(error); 
+        })
+});
 
 server.post('/accounts', validatePost, (req, res) => {
     db('accounts').insert(req.body, 'id')
@@ -28,9 +39,24 @@ server.post('/accounts', validatePost, (req, res) => {
         .catch(error => {
             res.status(500).json(error);
         })
-})
+});
 
 // middleware:
+function validateId(req, res, next) {
+    db('accounts').where({id: req.params.id}) 
+        .then(results => {
+            if (results.length === 0) {
+                res.status(400).json({message: 'Invalid account id'});
+            } else {
+                next();
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+
 function validatePost(req, res, next) {
     if(!Object.keys(req.body).length) {
         res.status(400).json({message: 'missing post data'})
